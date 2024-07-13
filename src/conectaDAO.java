af54e6b5f9541c56b942bc268431dc65f1c86b88
“Fabio <aoliveirafabio1@gmail.com>
2024-07-13 17:08:24 -0300

    Conexão com o banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -18,14 +18,30 @@ import javax.swing.JOptionPane;
 public class conectaDAO {
     
     public Connection connectDB(){
+        String url = "jdbc:mysql://localhost:3306/leilao_db";
+        String user = "root";
+        String password = "Convergente1";
+        
         Connection conn = null;
-        
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            java.util.Properties config = new java.util.Properties();
+            config.setProperty("user", user);
+            config.setProperty("password", password);
+            config.setProperty("useSSL", "true");
+            config.setProperty("verifyServerCertificate", "false");
+            
+            conn = DriverManager.getConnection(url,config);
+            
+            if(conn != null){
+                System.out.println("Conexão bem-sucessida!");
+            }
+            
+        } catch (ClassNotFoundException erro){
+            System.out.println("Erro ao conectar ao banco de dados." + erro.getMessage());
+        } catch(SQLException erro){
+            System.out.println("Falha na Conexão." + erro.getMessage());
         }
         return conn;
     }
